fcf931fdffeec6766599bae4468d30f59eaf462c
Samuel <s.wright@cgi.com>
2020-03-26 21:04:57 +0100

    BETTER STYLE FOR BETTER LIFE

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import styled from 'styled-components'
 
+const StyledDiv = styled.div`
+  padding: 15px;
+  margin-top: 15px;
+`
+
 const StyledInput = styled.input`
   border: 1px solid #b21f66;
   height: 35px;
-  width: 92%;
-  margin-top: 15px;
-  margin-left: 15px;
+  width: 100%;
   outline: none;
   padding: 10px;
   box-sizing: border-box;
@@ -16,7 +19,7 @@ const StyledInput = styled.input`
 const SearchBar = ({
     onChange = null
 }) => {
-    return <StyledInput type='text' name='searchBar' onChange={onChange}/>
+    return <StyledDiv><StyledInput type='text' name='searchBar' onChange={onChange}/></StyledDiv>
 }
 
 export default SearchBar
